4eab99040d70b6c30fe7c87092db11718ff88ebe
DarkSoul26 <sanchitkalsi@gmail.com>
2022-12-30 17:48:01 +0530

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image
+        source={require("./assets/logo.png")}
+        style={{ width: 80, height: 115 }}
+      />
+      <Text style={{ fontSize: 25, margin: 15 }}>How is your pain today?</Text>
+      <Text style={{ fontSize: 15, color: "484747" }}>
+        Kindly use the pain assesment scale below
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +20,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    marginTop: 50,
+    padding: 20,
+    // justifyContent: "center",
   },
 });
